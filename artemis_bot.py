7a161f91ca988de63d255097eaaf4012ca25b3e8
jeffreydrew <98627285+jeffreydrew@users.noreply.github.com>
2023-02-12 10:27:57 -0500

    reconfig

---
+++ artemis_bot.py
@@ -0,0 +1,45 @@
+import discord
+import config
+import responses
+
+
+intents = discord.Intents.default()
+intents.message_content = True
+
+async def send_message(message, user_message, is_private):
+    try:
+        response = responses.handle_responses(user_message)
+        if is_private:
+            await message.author.send(response)
+        else:
+            await message.channel.send(response)
+    except Exception as e:
+        print(e)
+
+
+def run_artemis():
+    token = config.TOKEN
+    client = discord.Client(intents=intents)
+
+    @client.event
+    async def on_ready():
+        print(f'{client.user} has connected to Discord!')
+
+    @client.event
+    async def on_message(message):
+        if message.author == client.user:
+            return
+
+        username = str(message.author)
+        user_message = str(message.content)
+        channel = str(message.channel)
+
+        print(f'{username} sent a message in {channel}: {user_message}')
+
+        if user_message[0] == '?':
+            user_message = user_message[1:]
+            await send_message(message, user_message, True)
+        else:
+            await send_message(message, user_message, False)
+
+    client.run(token)
---
+++ config.py
@@ -0,0 +1,14 @@
+PAPER_KEY = "PKKZSVR4ICWFB6YOHU04"
+PAPER_SECRET = "zaOMUgrElVp1ZFlchRIqM0oaOXlmcwePlifWKBdK"
+PAPER_URL = "https://paper-api.alpaca.markets"
+
+#Artemis Discord Bot
+APPLICATION_ID = '1074194991599071303'
+PUBLIC_KEY = '3ddefa610450ce93f1405bbe00e89bcb2fc80ca3f0eedd01e60fbd6e0aadbca3'
+CLIENT_ID = '1074194991599071303'
+
+INVITE_URL = 'https://discord.com/api/oauth2/authorize?client_id=1074194991599071303&permissions=1634235578432&scope=bot'
+
+TOKEN = 'MTA3NDE5NDk5MTU5OTA3MTMwMw.GDlMDz.YHrtENSw9RWbXm-5n78_roOS8Fg66KHuy5Tbvk'
+
+USERNAME = 'Artemis_bot#4990'
