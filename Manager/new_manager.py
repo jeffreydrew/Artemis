04baa707f3bc8c31a77c9218c655af3df5c25cef
Jeffrey Drew <98627285+jeffreydrew@users.noreply.github.com>
2023-03-10 21:34:41 -0500

    beginning production phase

--- Manager/new_manager.py
+++ Manager/new_manager.py
@@ -4,30 +4,74 @@ import talib
 sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 
 import pandas as pd
-import sqlite3
 from config import *
-import requests
-
+import yfinance as yf
+import alpaca_trade_api as tradeapi
+        
 
 class NewManager:
-    def __init__(self):
-        self.bars = []
+    def __init__(self, symbol, period, interval):
+        self.api = tradeapi.REST(PAPER_KEY, PAPER_SECRET)
+        self.account = self.api.get_account()
+
+        #stock specific
+        self.symbol = symbol
+        self.period = period
+        self.interval = interval
+
+        #strategy specific
+        self.stoploss = 0.0015
+        self.risk_coefficient = 0.45
+
+        #data (that gets updated every check, per period)
+        self.bars = self.get_bars()
+        self.macdhists = self.get_macds()
+
+    def get_bars(self):
+        # get data from yf
+        bars = yf.Ticker(self.symbol).history(self.period, self.interval)
+        return bars
+
+    def get_macds(self):
+        # macd signal
+	    # consider using macd on open price instead of close, backtest on this
+        _, _, macdhist = talib.MACD(self.bars["Close"])
+        return macdhist
+    
+    def strategy_macd(self):
+        #if price is less than stoploss, sell everything
+        #reconsider stoploss implementation !!!!
+        
+        price = self.bars.iloc[-1]["close"]
+        if price < self.last_buy_price * (1 - self.stop_loss):
+            self.sell(self.portfolio, price)
+
+        #look at the last two macdhist values
+        first, second = self.macdhists[-2:]
+       
+
+        #buy signal
+        if first > 0 and second < 0:
+            return "buy"
+        
+        #sell signal with stop loss
+        if first < 0 and second > 0:
+            return "sell"
+
+    def market_buy(self, qty):
+        pass
+
+    def market_sell(self, qty):
+        pass
+
+
+
+
+    def see_macds(self):
+        #write to csv
+        self.macdhists.to_csv("macdhist.csv")
 
 
 # driver temp
 if __name__ == "__main__":
-    function = "TIME_SERIES_INTRADAY"
-    symbol = "SPY"
-    interval = "1min"
-    apikey = ALPHA_VANTAGE_API_KEY
-    reqString = f"https://www.alphavantage.com/query?function={function}&symbol={symbol}&interval={interval}&apikey={apikey}"
-    print(reqString)
-    r = requests.get(
-        f"https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY&symbol=IBM&interval=5min&apikey={apikey}"
-    )
-
-    print(r.json()["Time Series (5min)"])
-    df = pd.DataFrame(r.json()["Time Series (5min)"])
-    #invert the rows and columns
-    df = df.transpose()
-    print(df)
+    m = NewManager("SPY", "1d", "1m")
